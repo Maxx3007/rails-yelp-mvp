1af6319e94be26cef7e287d54edc3420e37aa942
Maxim van Hoorn <mvanhoorn1@babson.edu>
2020-02-19 16:09:49 +0100

    router, controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,45 @@
 #
 # Examples:
 #
+Restaurant.destroy_all
+
+restaurant_examples = [ {
+  name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '073754287',
+    category: "chinese"
+},
+
+{
+  name:         'Disho',
+    address:      '9 Boundary St, London E2 7JE',
+    phone_number:  '073754288',
+    category: "chinese"
+},
+
+{
+  name:         'Dish',
+    address:      '11 Boundary St, London E2 7JE',
+    phone_number:  '073754285',
+    category: "chinese"
+},
+
+{
+  name:         'Dis',
+    address:      '21 Boundary St, London E2 7JE',
+    phone_number:  '073753385',
+    category: "chinese"
+},
+
+{
+  name:         'sh',
+    address:      '439 Boundary St, London E2 7JE',
+    phone_number:  '073224285',
+    category: "chinese"
+}
+]
+
+Restaurant.create!(restaurant_examples)
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
